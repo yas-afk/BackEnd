6c5469590f4ce058dda82c2796f0eae7264177d1
yasmin <yasminsoaresfarias32@gmail.com>
2025-04-07 17:03:05 -0300

    Update dbadicionarLivros.php

--- php/Atividades/projetolivro/pages/listasLivros/dbadicionarLivros.php
+++ php/Atividades/projetolivro/pages/listasLivros/dbadicionarLivros.php
@@ -3,14 +3,17 @@
 $nome = mysqli_real_escape_string($conexao,$_POST['nome']);
 $descricao = mysqli_real_escape_string($conexao,$_POST['descricao']);
 $avaliacao = mysqli_real_escape_string($conexao,$_POST['avaliacao']);
+$imagem = mysqli_real_escape_string($conexao,$_POST['imagem']);
 
 $sql = "INSERT INTO livros(
 nomeLivro
 descricaoLivro
-avaliacaoLivro,
+avaliacaoLivro
+imagemLivro,
 )VALUES(
  '{$nome}',
  '{$descricao}',
- '{$avaliacao}'
+ '{$avaliacao}',
+ '{$imagem}'
 )"
 ?>
